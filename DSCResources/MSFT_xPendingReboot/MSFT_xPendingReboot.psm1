e263ab56dcd1fc58fba7b3ad6d07a4349c667275
Chirishman <Chirishman@users.noreply.github.com>
2018-07-20 17:38:48 -0400

    Maintenance Window seperated from psm1

--- DSCResources/MSFT_xPendingReboot/MSFT_xPendingReboot.psm1
+++ DSCResources/MSFT_xPendingReboot/MSFT_xPendingReboot.psm1
@@ -1,31 +1,4 @@
-﻿Class MaintenanceWindow {
-
-    [Bool]$Enabled = $true
-    [DateTime]$StartTime
-    [DateTime]$EndTime
-    [System.DayOfWeek[]]$DaysOfWeek = @([System.DayOfWeek]::Saturday, [System.DayOfWeek]::Sunday)
-    [ValidateSet('Weekly', 'Daily')][string]$Frequency
-
-    MaintenanceWindow ([DateTime]$StartTime, [DateTime]$EndTime, [String]$Frequency) {
-        $this.StartTime = $StartTime
-        $this.EndTime = $EndTime
-        $this.Frequency = $Frequency
-
-        if ( $Frequency -eq 'Daily' ) {
-            $this.DaysOfWeek = ([system.dayofweek].DeclaredMembers.Name | Where-Object {$_ -notmatch '_'})
-        }
-    }
-
-    MaintenanceWindow ([DateTime]$StartTime, [DateTime]$EndTime, [String]$Frequency, [System.DayOfWeek[]]$DaysOfWeek) {
-        $this.StartTime = $StartTime
-        $this.EndTime = $EndTime
-        $this.DaysOfWeek = $DaysOfWeek
-        $this.Frequency = $Frequency
-    }
-
-}
-
-Function Get-TargetResource {
+﻿Function Get-TargetResource {
     [CmdletBinding()]
     [OutputType([Hashtable])]
     param
